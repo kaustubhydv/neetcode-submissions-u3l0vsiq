95c024354a735beda107216c081c00b83c4c4ae1
Kaustubh Yadav <81812619+kaustubhydv@users.noreply.github.com>
2026-04-24 23:29:24 +0530

    Add: climbing-stairs - submission-2

---
+++ Data Structures & Algorithms/climbing-stairs/submission-2.py
@@ -0,0 +1,11 @@
+class Solution:
+    def climbStairs(self, n: int) -> int:
+        cache = {}
+        def memoization(n, cache):
+            if n <= 1:
+                return 1
+            if n in cache:
+                return cache[n]
+            cache[n] = memoization(n-1, cache) + memoization(n-2, cache)
+            return cache[n]
+        return memoization(n, cache)
