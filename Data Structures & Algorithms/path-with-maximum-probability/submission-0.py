93a0b999e553ded3ec900d37975afc43ae5f6f1d
Kaustubh Yadav <81812619+kaustubhydv@users.noreply.github.com>
2026-05-26 12:12:54 +0530

    Add: path-with-maximum-probability - submission-0

---
+++ Data Structures & Algorithms/path-with-maximum-probability/submission-0.py
@@ -0,0 +1,23 @@
+class Solution:
+    def maxProbability(self, n: int, edges: List[List[int]], succProb: List[float], start_node: int, end_node: int) -> float:
+        adjL = defaultdict(list)
+        for i in range(len(edges)):
+            adjL[edges[i][0]].append((edges[i][1], succProb[i]))
+            adjL[edges[i][1]].append((edges[i][0], succProb[i]))
+        print(adjL)
+        largest = {}
+        maxHeap = [(-1, start_node)]
+        while maxHeap:
+            prob, node = heapq.heappop(maxHeap)
+            if node in largest:
+                continue
+            else:
+                largest[node] = -prob
+            for node1, prob1 in adjL[node]:
+                if node1 not in largest:
+                    heapq.heappush(maxHeap, (prob1*prob, node1))
+        return largest[end_node] if end_node in largest else 0
+
+        
+
+        
