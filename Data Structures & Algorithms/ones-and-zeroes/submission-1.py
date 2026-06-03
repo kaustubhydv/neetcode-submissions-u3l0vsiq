b526b1edcf7bd0f06c082d87fb5d123b3e82546f
Kaustubh Yadav <81812619+kaustubhydv@users.noreply.github.com>
2026-06-03 16:43:37 +0530

    Add: ones-and-zeroes - submission-1

---
+++ Data Structures & Algorithms/ones-and-zeroes/submission-1.py
@@ -0,0 +1,17 @@
+class Solution:
+    def findMaxForm(self, strs: List[str], m: int, n: int) -> int:
+        memo = {}
+        size = len(strs)
+        def dfs(i, currm, currn):
+            if i >= size:
+                return 0
+            if (i, currm, currn) in memo:
+                return memo[(i, currm, currn)]
+            res = dfs(i+1, currm, currn)
+            zero = strs[i].count('0')
+            one = strs[i].count('1')
+            if zero <= currm and one <= currn:
+                res = max(res, 1+dfs(i+1, currm-zero, currn-one))
+            memo[(i, currm, currn)] = res
+            return memo[(i, currm, currn)]
+        return dfs(0, m, n)
